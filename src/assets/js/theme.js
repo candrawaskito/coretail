9c9c677a66a9d24a8b799fc87bd3ef835fb571a8
HeyFrdy <candradwiwaskito90@gmail.com>
2024-07-16 16:14:46 +0700

    initial

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -44,75 +44,12 @@
       });
    }
 
-   // Performance Chart
-   var perfomanceChart = document.getElementById("perfomanceChart");
-
-   if (perfomanceChart) {
-      var options = {
-         series: [100, 78, 89],
-         chart: {
-            height: 320,
-            type: "radialBar",
-         },
-         colors: ["#28a745", "#ffc107", "#dc3545"],
-         stroke: {
-            lineCap: "round",
-         },
-         plotOptions: {
-            radialBar: {
-               startAngle: -168,
-               endAngle: -450,
-               hollow: {
-                  size: "55%",
-               },
-               track: {
-                  background: "transparent",
-               },
-               dataLabels: {
-                  show: false,
-               },
-            },
-         },
-      };
-
-      var chart = new ApexCharts(perfomanceChart, options);
-      chart.render();
-   }
-
    // Offcanvas
    var offcanvasElements = document.querySelectorAll(".offcanvas");
 
    if (offcanvasElements.length) {
       const offcanvasElementList = document.querySelectorAll(".offcanvas");
       const offcanvasList = [...offcanvasElementList].map((offcanvasEl) => new bootstrap.Offcanvas(offcanvasEl));
-   }
-
-   // Dropzone
-   var dropzoneElements = document.querySelectorAll("#my-dropzone");
-
-   if (dropzoneElements.length) {
-      Dropzone.autoDiscover = false;
-
-      const myDropzone = new Dropzone("#my-dropzone", {
-         url: "https://httpbin.org/post",
-         maxFilesize: 5,
-         acceptedFiles: "image/*",
-         addRemoveLinks: true,
-         autoProcessQueue: true,
-      });
-
-      // Add event listeners
-      myDropzone.on("addedfile", function (file) {
-         console.log("File added: " + file.name);
-      });
-
-      myDropzone.on("removedfile", function (file) {
-         console.log("File removed: " + file.name);
-      });
-
-      myDropzone.on("success", function (file, response) {
-         console.log("File uploaded successfully:", response);
-      });
    }
 
    // Tooltips
